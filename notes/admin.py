da9a0e86acabc39438a509970f2bd10a4b971b33
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-19 13:52:50 +0300

    add: register models

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer,Subject,Note
 
+
+admin.site.register(Customer)
+admin.site.register(Subject)
+admin.site.register(Note)
 # Register your models here.
