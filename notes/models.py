f1ab1c3767328a1c7892eeb2e2ff16acbd462ea7
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-19 13:50:41 +0300

    add: Customer,Notes and Subject models

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,24 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+
+
+class Notes(models.Model):
+    topic = models.CharField(max_length=30)
+    Notes = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=CASCADE)
+    subject = models.ForeignKey('Subject', on_delete=CASCADE)
+
+
+class Subject(models):
+    subject = models.CharField(max_length=20)
+
+
+
+
+# Create your models her
