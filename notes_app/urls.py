78a3889cb711246855e8614fd29bf0af2f8098cf
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-21 13:22:38 +0300

    add: reload functionality to reload when tailwind css changes

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from notes.views import list_create_subject
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/',list_create_subject),
+    path("__reload__/", include("django_browser_reload.urls"))
 ]
