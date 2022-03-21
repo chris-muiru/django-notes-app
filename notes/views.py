728cebf029a036b6493beff6baca84f4ec23f2cf
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-21 13:17:59 +0300

    add: view to handle subject creation

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Subject
+from .forms import SubjectForm
 
+
+def list_create_subject(request):
+    form = SubjectForm()
+    if request.method == 'POST':
+        print(request.POST)
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            print('it is valid')
+            form.save()
+        else:
+            print('not')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'notes/subject_pages/subject.html', context)
+
+
+# Crud architecture for
 # Create your views here.
