399a32792269d0b3b2860af309bd808c1a421afb
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-24 10:16:51 +0300

    add: add auto_now attr to date_created,add __str__ dunder functions

--- notes/models.py
+++ notes/models.py
@@ -1,23 +1,31 @@
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.timezone import now
+# from django.utils.timezone import now
 
 
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.user.username}'
 
 class Note(models.Model):
     topic = models.CharField(max_length=30)
     Notes = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateField(auto_now=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.topic}'
 
 
 class Subject(models.Model):
     name = models.CharField(max_length=20)
-    date_created = models.DateField(default=now)
+    date_created = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.name}'
 
 # Create your models her
