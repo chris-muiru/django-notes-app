12ca3ae5614259d131e2ccda74e7d3751800053a
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-24 10:25:04 +0300

    add: notes urls

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from notes.views import list_create_subject
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/',list_create_subject),
-    path("__reload__/", include("django_browser_reload.urls"))
+    path("__reload__/", include("django_browser_reload.urls")),
+    path('', include('notes.urls'))
 ]
