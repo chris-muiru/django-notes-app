1a09f6efd85786bbab27591ff61a6b3d47b8c028
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-24 10:20:34 +0300

    add: add create_notes_view and dashboard_view

--- notes/views.py
+++ notes/views.py
@@ -1,24 +1,53 @@
-from django.shortcuts import render
-from .models import Subject
-from .forms import SubjectForm
+from django.shortcuts import render, redirect
+from .models import Subject, Note
+from .forms import SubjectForm, createNotesForm
 
 
-def list_create_subject(request):
+def list_create_subject_view(request):
     form = SubjectForm()
     if request.method == 'POST':
-        print(request.POST)
         form = SubjectForm(request.POST)
-        if form.is_valid():
-            print('it is valid')
+        if form.is_valid() and not Subject.objects.filter(name=form.cleaned_data['name']).exists():
             form.save()
-        else:
-            print('not')
+    subject_query_set = Subject.objects.all()
+    context = {
+        'form': form,
+        'subjects': subject_query_set
+    }
+    return render(request, 'subject_page/subject.html', context)
 
+
+def create_notes_view(request):
+    form = createNotesForm()
+    if request.method == 'POST':
+        form = createNotesForm(request.POST)
+        form.save()
+        return redirect('dashboard')
     context = {
         'form': form
     }
-    return render(request, 'notes/subject_pages/subject.html', context)
+    return render(request, 'notes/createNote.html', context)
 
+
+def dashboard_view(request):
+    note_queryset = Note.objects.all()
+    content = {
+        'notes': note_queryset
+    }
+    return render(request, 'notes/dashboard.html', content)
+
+
+def note_view(request, pk):
+    query_note = Note.objects.filter(id=pk).get()
+    print(query_note)
+
+    if request.method =='POST':
+        query_note.delete()
+        return redirect('dashboard')
+    context = {
+        'note': query_note
+    }
+    return render(request, 'notes/note.html', context)
 
 # Crud architecture for
 # Create your views here.
