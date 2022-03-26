d9454d85f6df1a926586fed840a90826f29cdd6b
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-26 13:25:56 +0300

    refactor: change view name

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import list_create_subject_view, create_notes_view, dashboard_view, note_view
+from .views import list_create_subject_view, create_notes_view, dashboard_view,ListDeleteNoteView
 
 urlpatterns = [
     path('dashboard/', dashboard_view, name="dashboard"),
     path('create-subject/', list_create_subject_view, name="create-subject"),
     path('create-note/', create_notes_view, name='create-note'),
-    path('note/<int:pk>/',note_view,name='note')
+    path('note/<int:pk>/',ListDeleteNoteView,name='note')
 ]
