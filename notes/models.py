c554cae6ad7491da825a314efc584a576a496eda
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-26 13:27:06 +0300

    remove: remove model and place it in authenticate app

--- notes/models.py
+++ notes/models.py
@@ -1,14 +1,7 @@
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
-# from django.utils.timezone import now
-
-
-class Customer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.user.username}'
+from authenticate.models import Customer
 
 class Note(models.Model):
     topic = models.CharField(max_length=30)
