ab686599214f1ff7160e693882c6933dc7677c3f
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-26 13:24:40 +0300

    add: login,logout and register user views

---
+++ authenticate/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterUserForm
+from django.contrib.auth import login, authenticate, logout
+
+
+def registerView(request):
+    form = RegisterUserForm()
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'authenticate/signup.html', context)
+
+# login user
+
+
+def logIn(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            pass
+
+    context = {}
+    return render(request, 'authenticate/signin.html', context)
+
+def logOut(request):
+    logout(request)
+    return redirect('login')
+
+
+
+# Create your views here.
