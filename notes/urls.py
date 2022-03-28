5dfad87bb06b00cd1ca229f3774bc2f0a30281e1
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-28 23:03:42 +0300

    add: urlpatterns where media files are located

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import list_create_subject_view, create_notes_view, dashboard_view,ListDeleteNoteView
+from .views import create_notes_view, dashboard_view,ListDeleteNoteView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', dashboard_view, name="dashboard"),
-    path('create-subject/', list_create_subject_view, name="create-subject"),
     path('create-note/', create_notes_view, name='create-note'),
     path('note/<int:pk>/',ListDeleteNoteView,name='note')
 ]
+# path('create-subject/', list_create_subject_view, name="create-subject"),
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
