fe058a1524a2ab8e3fe420057e85ce93bdac237d
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-28 22:54:18 +0300

    add: change model from Customer to User

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import Customer
 from django import forms
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
-        model = Customer
+        model = User
         fields = ['username', 'email', 'password1', 'password2']
 
 
+class SignInForm(forms.Form):
+    username = forms.CharField()
+    password = forms.PasswordInput()
