0ed807d2087c5f08254dd07ee3897e641f453160
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-28 22:55:40 +0300

    add: add profile model with a onetoone rship with user

--- authenticate/models.py
+++ authenticate/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import  User
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user=models.OneToOneField(User,null=True,on_delete=models.CASCADE) 
+    image =models.ImageField(default='default.jpeg',upload_to='profile_pics')   
 
+    def __str__(self):
+        return f"{self.user.username} profile"
 
-# user model
-class Customer(User):
-    pass
 
 # Create your models here.
