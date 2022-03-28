6b91451d60ffe2e52304f4814f8f864278f51c3b
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-28 23:02:53 +0300

    add: subject field to replace subject model

--- notes/models.py
+++ notes/models.py
@@ -1,24 +1,23 @@
-from tkinter import CASCADE
 from django.db import models
-from django.contrib.auth.models import User
-from authenticate.models import Customer
+from django.contrib.auth.models import User 
 
 class Note(models.Model):
     topic = models.CharField(max_length=30)
     Notes = models.TextField()
     date_created = models.DateField(auto_now=True)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=20)
 
     def __str__(self):
         return f'{self.topic}'
 
 
-class Subject(models.Model):
-    name = models.CharField(max_length=20)
-    date_created = models.DateField(auto_now=True)
-    
-    def __str__(self):
-        return f'{self.name}'
+
+# class Subject(models.Model):
+#     name = models.CharField(max_length=20)
+#     date_created = models.DateField(auto_now=True)
+
+#     def __str__(self):
+#         return f'{self.name}'
 
 # Create your models her
