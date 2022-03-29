bc58c84912d0be0ee7a6c302ea7a7d7716fa66b8
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-29 19:20:56 +0300

    add: profileform

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from .models import Profile
 
 
 class RegisterUserForm(UserCreationForm):
@@ -10,6 +11,7 @@ class RegisterUserForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
-class SignInForm(forms.Form):
-    username = forms.CharField()
-    password = forms.PasswordInput()
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('image',)
