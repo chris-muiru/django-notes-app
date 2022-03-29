b35b3354d7d155a9dec5dc28d924431b8580e03b
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-29 19:23:16 +0300

    add: can_delete decorator to only allow authors to delete note

---
+++ notes/decorators.py
@@ -0,0 +1,13 @@
+from .models import Note
+from django.shortcuts import redirect
+# delete file ba only if user
+
+
+def can_delete(func):
+    def wrapper_can_delete(request, pk, *args, **kwargs):
+        if Note.objects.get(id=pk).user.username == request.user:
+            return func(request, pk, *args, **kwargs)
+        else:
+            return redirect('dashboard')
+
+    return wrapper_can_delete 
