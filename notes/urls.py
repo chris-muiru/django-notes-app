a35b1faa9dec03526ba9852444cfa41e10c54fb5
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-29 19:25:58 +0300

    add: delete endpoint for deletion of notes

--- notes/urls.py
+++ notes/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import create_notes_view, dashboard_view,ListDeleteNoteView
+from .views import create_notes_view, dashboard_view,ListNoteView,DeleteNoteView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', dashboard_view, name="dashboard"),
     path('create-note/', create_notes_view, name='create-note'),
-    path('note/<int:pk>/',ListDeleteNoteView,name='note')
+    path('note/<int:pk>/',ListNoteView,name='note'),
+    path('delete/<int:pk>/',DeleteNoteView,name='delete-note')
 ]
 # path('create-subject/', list_create_subject_view, name="create-subject"),
 
