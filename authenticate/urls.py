45c8507a893de864af2e76be10021cae48ee2fd5
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-29 19:22:12 +0300

    add: add profile url

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import registerView,logIn,logOut
+from .views import registerView, logIn, logOut, updateProfile
 urlpatterns = [
     path('sign-up/', registerView, name='register'),
     path('sign-in/', logIn, name='login'),
-    path('logout/',logOut,name="logout")
+    path('logout/', logOut, name="logout"),
+    path('profile/', updateProfile, name='profile')
 ]
