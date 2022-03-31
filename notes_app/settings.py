c330ca77d522ce3c366a1bbbc957fd2f1d54c1df
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-31 17:35:31 +0300

    update: update settings.py

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('NOTES_APP_SECRET_KEY')
+SECRET_KEY = 'django-insecure-$%795n2$z0j2nphf+0$7^%&m=k16v6(7)@*3rv94-)$g90j)9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
