7ade096c7b60fc9c3de79aefebe42dd18c5f8c64
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-04-01 00:04:29 +0300

    update: add django-reroku to automatically configure

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -12,15 +12,17 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+
+import django_heroku 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$%795n2$z0j2nphf+0$7^%&m=k16v6(7)@*3rv94-)$g90j)9u'
+# SECRET_KEY = 'django-insecure-$%795n2$z0j2nphf+0$7^%&m=k16v6(7)@*3rv94-)$g90j)9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
