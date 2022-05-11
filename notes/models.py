b2dc5e5f39cac9f618b6d501dd7ea93a08ea2616
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-11 16:59:51 +0300

    refactor: remove subject model

--- notes/models.py
+++ notes/models.py
@@ -13,11 +13,3 @@ class Note(models.Model):
 
 
 
-# class Subject(models.Model):
-#     name = models.CharField(max_length=20)
-#     date_created = models.DateField(auto_now=True)
-
-#     def __str__(self):
-#         return f'{self.name}'
-
-# Create your models her
