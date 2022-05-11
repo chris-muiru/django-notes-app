e2ea64597dba2e63d92f666ba9298ea0ac5a6be6
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-11 16:50:17 +0300

    add: return the username and image location in __str__

--- authenticate/models.py
+++ authenticate/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user=models.OneToOneField(User,null=True,on_delete=models.CASCADE) 
-    image =models.ImageField(default='default.jpeg',upload_to='profile_pics')   
+    image =models.ImageField(default='default.jpg',upload_to='profile_pics')   
 
     def __str__(self):
-        return f"{self.user.username} profile"
+        return f"{self.user.username} - {self.image}"
 
 
 # Create your models here.
