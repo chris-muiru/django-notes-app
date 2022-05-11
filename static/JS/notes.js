3e6421ff3e5599d3b54a5f2ede929735080a5429
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-11 17:04:44 +0300

    add: static javascript

--- static/JS/notes.js
+++ static/JS/notes.js
@@ -1,14 +1,12 @@
 let btnMenu = document.getElementById('btn-menu');
 let divLinks = document.getElementById('div-links');
-
 const setDivState=()=>{
     if(divLinks.style.display==""){ 
-        divLinks.style.display="block";
+        divLinks.style.display="block"
     }
     else if(divLinks.style.display=="block"){
         divLinks.style.display="none"
     }
-
 }
 
-btnMenu.addEventListener('click',setDivState)
+btnMenu.addEventListener('click',setDivState);
